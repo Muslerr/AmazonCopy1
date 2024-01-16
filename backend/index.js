338258627b05b7928a66a941dd19f3d6fc5eaaf7
muslerr <muslerbar@gmail.com>
2024-01-16 12:40:07 +0200

    change 1

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,11 @@ import express from "express";
 import cors from "cors";
 
 const app = express();
-
+const port = 8080;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
+
+app.listen(port,function(){
+    console.log("lisstining on ", port);
+});
