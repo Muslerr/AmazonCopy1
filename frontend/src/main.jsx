e1896c3017f76bbbadc1c712f66c4bd79a580b25
Muslerr <132832406+Muslerr@users.noreply.github.com>
2024-03-10 21:07:20 +0200

    try 3

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -9,6 +9,7 @@ import { StoreProvider } from "./Store.jsx";
 
 axios.defaults.baseURL = import.meta.env.DEV ? 'http://localhost:8080' : 'https://amazon-copy1.vercel.app/';
 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
   <StoreProvider>
